5cb03b19384733bd13f5fbfdd0e131c122c9a656
William Fong <trigition@gmail.com>
2015-04-13 16:27:30 -0600

    Worked on extracting columns from csv file

--- dataProc.py
+++ dataProc.py
@@ -25,6 +25,7 @@ Begin_Latitude = 45
 Begin_Longitude = 46
 End_Latitude = 47
 End_Longitude = 48
+
 def isInt(s):
     try:
         int(s)
@@ -40,6 +41,10 @@ def series_fromFile(filename, header_index):
     FullPath = './data/NOAA_StormData/uncompressed/' + filename
     return pd.Series.from_csv(FullPath, index_col=header_index)
 
+def csvFromSeries(filename, series):
+    FullPath = './data/NOAA_StormData/uncompressed/' + filename
+    series.to_csv(FullPath)
+
 def printRow(row):
     Month = row[1]['MONTH_NAME']
     Year = row[1]['YEAR']
@@ -53,38 +58,5 @@ def print_csv_columns(df):
 
 pd.set_option('display.max_rows', 10)
 pd.set_option('display.width', 1000)
+input_file = 'concatedDataAll'
 
-sortingDict = {'January': 1, 'February': 2, 'March': 3, 'April': 4,
-               'May': 5, 'June': 6, 'July': 7, 'August': 8,
-               'September': 9, 'October': 10, 'November': 11, 'December': 12}
-
-<<<<<<< HEAD
-data = openDataFile('concatedDataAll')
-#data['MONTH_RANK'] = data['MONTH_NAME'].map(sortingDict)
-data = data.sort(columns=['YEAR', 'MONTH_RANK'], ascending=True)
-
-for row in data.iterrows():
-    printRow(row)
-=======
-convertedData = pd.DataFrame(columns=['eventID', 'episodeID', 'begin_lat', 'begin_long',
-                                      'end_lat', 'end_long', 'begin_day', 'month',
-                                      'end_day', 'year', 'stormType', 'propertyDamage'])
-data = df_fromFile('concatedDataAll')
-#for row in data.iterrows():
-    #Determine Time Parameters
-    #beginDay = row[1]['BEGIN_DAY']
-    #endDay = row[1]['END_DAY']
-    #month = row[1]['MONTH_NAME']
-    #year = row[1]['YEAR']
-    #eventID = row[1]['EVENT_ID']
-    #episodeID = row[1]['EPISODE_ID']
-    #beginLat = row[1]['BEGIN_LAT']
-    #beginLong = row[1]['BEGIN_LON']
-    #endLat = row[1]['END_LAT']
-    #endLong = row[1]['END_LON']
-    #stormType = row[1]['EVENT_TYPE']
-    #damage = row[1]['DAMAGE_PROPERTY']
-    #print "On:", month, ",", year, "a", stormType, "occured."
-stormTypes = series_fromFile('concatedDataAll', Storm_Type)
-print stormTypes
->>>>>>> 15a26f476b3632845a168b8a5af57531664cf77d
